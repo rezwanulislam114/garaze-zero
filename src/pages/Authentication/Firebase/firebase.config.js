6d4b13a5adc5b54de09715c2aabbd82818627f49
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-11-10 15:57:53 +0600

    router setup

---
+++ src/pages/Authentication/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPPzx_dx_iAQhMkw0UV2TmoI1yRJkTDDE",
+    authDomain: "garagezero-6754f.firebaseapp.com",
+    projectId: "garagezero-6754f",
+    storageBucket: "garagezero-6754f.appspot.com",
+    messagingSenderId: "509315056680",
+    appId: "1:509315056680:web:3da5d971050081d3e2e11d"
+};
+
+export default firebaseConfig;
